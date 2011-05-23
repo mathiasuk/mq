4a947dab07532a67170435f1c278c7760e649ba2
Mathias Andre <mathias@acronycal.org>
2011-05-23 17:15:44 +0200

    Added initial __daemon_parse_line

--- daemon.c
+++ daemon.c
@@ -3,12 +3,16 @@
 #include <sys/epoll.h>
 #include <unistd.h>
 #include <limits.h>
+#include <string.h>
 
 #include "daemon.h"
 #include "logger.h"
 
 #define MAX_EVENTS	64
 #define TIMEOUT		1000
+
+/* Private methods: */
+static void __daemon_parse_line (Daemon * self, char * line);
 
 Daemon * daemon_new (void)
 {
@@ -42,7 +46,7 @@ void daemon_setup (Daemon * self)
 	/* Create the epoll fd: */
 	self->epfd = epoll_create (5);
 	if (self->epfd < 0) {
-		perror ("epoll_create");
+		perror ("daemon_setup:epoll_create");
 		exit (EXIT_FAILURE);
 	}
 
@@ -50,7 +54,7 @@ void daemon_setup (Daemon * self)
 	event.data.fd = self->pipe;
 	event.events = EPOLLIN;
 	if (epoll_ctl (self->epfd, EPOLL_CTL_ADD, self->pipe, &event) == -1) {
-		perror ("epoll_ctl");
+		perror ("daemon_setup:epoll_ctl");
 		exit (EXIT_FAILURE);
 	}
 }
@@ -61,11 +65,9 @@ void daemon_run (Daemon * self)
 	char buf[LINE_MAX], * s;
 	int i, nr_events, len;
 	 
-	logger_info(self->log, "In 'daemon_run'\n");
-
 	events = malloc (sizeof (struct epoll_event) * MAX_EVENTS);
 	if (!events) {
-		perror ("malloc");
+		perror ("daemon_run:malloc");
 		exit (EXIT_FAILURE);
 	}
 
@@ -75,7 +77,7 @@ void daemon_run (Daemon * self)
 	while (1) {
 		nr_events = epoll_wait (self->epfd, events, MAX_EVENTS, TIMEOUT);
 		if (nr_events < 0) {
-			perror ("epoll_wait");
+			perror ("daemon_run:epoll_wait");
 			exit (EXIT_FAILURE);
 		}
 
@@ -86,15 +88,16 @@ void daemon_run (Daemon * self)
 		for (i = 0; i < nr_events ; i++)
 		{
 			if (events[i].events == EPOLLIN) {
-				len = read (events[i].data.fd, s, sizeof(char));
+				len = read (events[i].data.fd, s, sizeof (char));
 				if (len == -1) { 
-					perror("read");
-					exit(EXIT_FAILURE);
+					perror ("daemon_run:read");
+					exit (EXIT_FAILURE);
 				} 
 				if (len) {
 					if (*s == '\n') {
 						*s = '\0';
-						logger_info(self->log, "Read line: %s\n", buf);
+						logger_debug (self->log, "Read line: '%s'\n", buf);
+						__daemon_parse_line(self, buf);
 						s = buf;
 					} else s++;
 				}
@@ -102,4 +105,41 @@ void daemon_run (Daemon * self)
 		}
 	}
 	return ;
+}
+
+
+/* Private methods: */
+
+static void __daemon_parse_line (Daemon * self, char * line)
+{
+	char * action;
+	char * line_d;
+
+	if (line == NULL)
+		return ;
+
+	/* Strip heading whitespaces: */
+	while (*line == ' ')
+		line++;
+
+	/* Strip trailing whitespaces: */
+	while (line[strlen (line)-1] == ' ')
+		line[strlen (line)-1] = '\0';
+
+	if (!strlen (line))
+		return ;
+
+	/* We duplicate the 'line' string as strtok modifies it: */
+	if ((line_d = strdup (line)) == NULL) {
+		perror ("__daemon_parse_line:strdup:");
+		exit (EXIT_FAILURE);
+	}
+	/* Parse the action (first word of the line): */
+	action = strtok (line_d, " ");
+
+	if (strcmp (action, "add") == 0) {
+	} else {
+		logger_warn (self->log, "Unknown action: '%s'\n", line);
+	}
+
 }
