bb9e9d4052d8a4e3446418f7d1f562693a42a174
Mathias Andre <mathias@acronycal.org>
2011-05-23 21:51:55 +0200

    Added logging of pipe and log paths

--- daemon.c
+++ daemon.c
@@ -27,9 +27,10 @@ Daemon * daemon_new (char * pipe_path, char * log_path)
 	}
 
 	/* Build the pipe's path: */
-	if (pipe_path)
+	if (pipe_path) {
 		daemon->pipe_path = pipe_path;
-	else {
+		logger_debug (self->log, "Using provided pipe's path: '%s'\n", daemon->pipe_path);
+	} else {
 		if ((home = getenv ("HOME")) == NULL) {
 			perror ("daemon_new:getenv");
 			exit (EXIT_FAILURE);
@@ -40,14 +41,16 @@ Daemon * daemon_new (char * pipe_path, char * log_path)
 			exit (EXIT_FAILURE);
 		}
 		sprintf (daemon->pipe_path, "%s/%s", home, PIPE_FILENAME);
+		logger_debug (self->log, "Using default pipe's path: '%s'\n", daemon->pipe_path);
 	}
 
 	daemon->pipe = -1;
 
 	/* Build the log file's path: */
-	if (log_path)
+	if (log_path) {
 		daemon->log_path = log_path;
-	else {
+		logger_debug (self->log, "Using provided log's path: '%s'\n", daemon->pipe_path);
+	} else {
 		if ((home = getenv ("HOME")) == NULL) {
 			perror ("daemon_new:getenv");
 			exit (EXIT_FAILURE);
@@ -58,6 +61,7 @@ Daemon * daemon_new (char * pipe_path, char * log_path)
 			exit (EXIT_FAILURE);
 		}
 		sprintf (daemon->log_path, "%s/%s", home, LOG_FILENAME);
+		logger_debug (self->log, "Using default log's path: '%s'\n", daemon->pipe_path);
 	}
 
 	daemon->log = NULL;
