9ab6321acbde7d3cf2d67f59148aed3f663d4770
Mathias Andre <mathias@acronycal.org>
2011-05-31 20:46:28 +0200

    Added Client object and TODO file

--- src/client.c
+++ src/client.c
@@ -1,44 +1,131 @@
-#include <stdio.h>
+/* 
+ * This file is part of mq.
+ * mq - src/client.c
+ * Copyright (C) 2011 Mathias Andre <mathias@acronycal.org>
+ *
+ * mq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
+#include <stdio.h>
+#include <limits.h>
 #include <sys/socket.h>
-#include <sys/un.h>
-#include <unistd.h>
 
-#define SOCK_PATH "/home/mandre/.mq.sock"
+#include "client.h"
+#include "daemon.h"
 
-int main(void)
+/* 
+ * Create and initialise the Cleint
+ * args:   void
+ * return: Client object or NULL on error
+ */
+Client * client_new (void)
 {
-    int s, t, len;
-    struct sockaddr_un remote;
-    char str[100];
+	Client * client = malloc (sizeof (Client));
+	char * home;
 
-    if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-        perror("socket");
-        exit(1);
+	/* Build the default pidfile's path */
+	if ((home = getenv ("HOME")) == NULL)
+		return NULL;
+	client->_pid_path = malloc (snprintf (NULL, 0, "%s/%s", home, 
+										  PID_FILENAME) + 1);
+	if (client->_pid_path == NULL)
+		return NULL;
+	sprintf (client->_pid_path, "%s/%s", home, PID_FILENAME);
+
+	/* Build the default socket's path */
+	if ((home = getenv ("HOME")) == NULL)
+		return NULL;
+	client->_sock_path = malloc (snprintf (NULL, 0, "%s/%s", home, 
+										   SOCK_FILENAME) + 1);
+	if (client->_sock_path == NULL)
+		return NULL;
+	sprintf (client->_sock_path, "%s/%s", home, SOCK_FILENAME);
+
+	/* Build the default logfile's path */
+	if ((home = getenv ("HOME")) == NULL)
+		return NULL;
+	client->_log_path = malloc (snprintf (NULL, 0, "%s/%s", home, 
+										  LOG_FILENAME) + 1);
+	if (client->_log_path == NULL)
+		return NULL;
+	sprintf (client->_log_path, "%s/%s", home, LOG_FILENAME);
+
+	return client;
+}
+
+/* 
+ * Setup the client
+ * args:   Client
+ * return: 0 on success, 1 on failure
+ */
+int client_setup (Client * self)
+{
+	/* TODO: parse arguments */
+	return 0;
+}
+
+/*
+ * Run the client
+ * args:   Client
+ * return: void
+ */
+void client_run (Client * self)
+{
+	extern Daemon * d;
+    int sock, len;
+    struct sockaddr_un remote;
+	char buf[LINE_MAX];
+
+
+	/* TODO: check if the daemon is already running otherwise start it */
+	d = daemon_new (self->_sock_path, self->_pid_path, self->_log_path);
+	if (d == NULL) {
+		perror ("client_run:daemon_new");
+		exit (EXIT_FAILURE);
+	}
+
+	if (daemon_setup (d) == 0) {
+		/* In daemon*/
+		daemon_run (d);	
+		exit (EXIT_FAILURE);	/* We should never reach this */
+	}
+
+    if ((sock = socket (AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror ("socket");
+        exit(EXIT_FAILURE);
     }
 
-    printf("Trying to connect...\n");
+    printf ("Trying to connect...\n");
 
     remote.sun_family = AF_UNIX;
-    strcpy(remote.sun_path, SOCK_PATH);
-    len = strlen(remote.sun_path) + sizeof(remote.sun_family);
-    if (connect(s, (struct sockaddr *)&remote, len) == -1) {
+    strcpy (remote.sun_path, self->_sock_path);
+    len = strlen (remote.sun_path) + sizeof (remote.sun_family);
+    if (connect (sock, (struct sockaddr *) &remote, len) == -1) {
         perror("connect");
         exit(1);
     }
 
-    printf("Connected.\n");
+    printf ("Connected.\n");
 
-    while(printf("> "), fgets(str, 100, stdin), !feof(stdin)) {
-        if (send(s, str, strlen(str), 0) == -1) {
-            perror("send");
-            exit(1);
+    while (printf("> "), fgets (buf, 100, stdin), !feof(stdin)) {
+        if (send (sock, buf, strlen (buf), 0) == -1) {
+            perror ("send");
+            exit (EXIT_FAILURE);
         }
 
-		/* if ((t=recv(s, str, 100, 0)) > 0) { */
+		/* if ((t=recv(sock, str, 100, 0)) > 0) { */
 			/* str[t] = '\0'; */
 			/* printf("echo> %s", str); */
 		/* } else { */
@@ -48,7 +135,7 @@ int main(void)
 		/* } */
     }
 
-    close(s);
+    close (sock);
 
-    return 0;
+    exit (EXIT_SUCCESS);
 }
---
+++ src/client.h
@@ -0,0 +1,40 @@
+/* 
+ * This file is part of mq.
+ * mq - src/client.h
+ * Copyright (C) 2011 Mathias Andre <mathias@acronycal.org>
+ *
+ * mq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#define PID_FILENAME ".mq.pid"
+#define SOCK_FILENAME ".mq.sock"
+#define LOG_FILENAME ".mq.log"
+
+typedef struct _Client Client;
+
+struct _Client 
+{
+	char * _pid_path;
+	char * _sock_path;
+	char * _log_path;
+};
+
+Client * client_new (void);
+int client_setup (Client * self);
+void client_run (Client * self);
+
+#endif /* CLIENT_H */
