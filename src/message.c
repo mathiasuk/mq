09aaa13ddb0d02fe6d9d285eae5f68f9aea21308
Mathias Andre <mathias@acronycal.org>
2011-06-03 09:23:12 +0200

    Added Message object

---
+++ src/message.c
@@ -0,0 +1,41 @@
+/* 
+ * This file is part of mq.
+ * mq - src/client.c
+ * Copyright (C) 2011 Mathias Andre <mathias@acronycal.org>
+ *
+ * mq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdlib.h>
+
+#include "message.h"
+
+/* 
+ * Create and initialise a message
+ * args:   message type, message string
+ * return: Message object or NULL on error
+ */
+Message * message_new (MessageType type, char * content, int sock)
+{
+	Message * message = malloc (sizeof (Message));
+
+	if (message == NULL)
+		return NULL;
+
+	message->_type = type;
+	message->_content = content;
+	message->_sock = sock;
+
+	return message;
+}
---
+++ src/message.h
@@ -0,0 +1,46 @@
+/* 
+ * This file is part of mq.
+ * mq - src/message.h
+ * Copyright (C) 2011 Mathias Andre <mathias@acronycal.org>
+ *
+ * mq is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mq is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mq.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+#define PID_FILENAME ".mq.pid"
+#define SOCK_FILENAME ".mq.sock"
+#define LOG_FILENAME ".mq.log"
+
+/* Type of messages sent to client */
+typedef enum {
+	OK,		/* exit successfully */
+	KO,		/* exit unsuccessfully */
+	OUT,	/* print message to stdout */
+	ERR		/* print message to stderr */
+} MessageType;
+
+typedef struct _Message Message;
+
+struct _Message 
+{
+	MessageType _type;
+	char * _content;	/* content of the message */
+	int _sock;			/* socket the message should be written to */
+};
+
+Message * message_new (MessageType type, char * message, int sock);
+
+#endif /* CLIENT_H */
